8d58734d79c187d948678d7631c5103335cb4e7e
BenHooter <benhootergaming@gmail.com>
2025-03-05 12:13:31 -0700

    Add files via upload

---
+++ nyxlumberjack/languages/de_lang.lua
@@ -0,0 +1,18 @@
+Locales["en_lang"] = {
+    Chop = 'Chop Tree',
+    Tree = 'Tree',
+    Durability = 'durability',
+    WorkingHere = 'Cut down a tree... ',
+    NoMoreInventorySpaceFor = 'No more space in the bag for ',
+    YouGot = 'You get ',
+    WHGot = ' Who Have received: ',
+    YouGotLuck = 'You are lucky and you also get',
+    WHGotLucky = 'you have gotten lucky',
+    ToolBroken = 'Tool Has Broke',
+    InHere = 'In ',
+    YouCantChop = ' You can Not chop',
+    NoToolInHand = 'no tool is in hand',
+    ToolIsNew = 'tool has been repaired',
+    NoRepairItem = 'you do not have tool to repair',
+    RepairingTool = 'Repairing tool',
+}
